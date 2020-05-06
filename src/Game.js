61652da6c735782b5383088f3dc4c399b7345b85
Bill Wilke <billwilke2@gmail.com>
2020-05-06 16:26:27 -0600

    Adds a currentRound property and start method

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,26 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round')
+
 
 class Game {
-  constructor() {}
+  constructor(round) {
+    this.currentRound = round
+  }
+  start() {
+    for(var i = 0; i < data.length; i++) {
+      let cards = new Card(data[i].id, data[i].question, data[i].answers, data[i].correctAnswer)
+    }
+    let deck = new Deck(cards);
+    let turn = new Turn(deck);
+    turn.returnCurrentCard()
+    this.printMessage();
+    this.printQuestion();
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
